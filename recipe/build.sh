0010009805771fecf62a3e3d611cc30d4b38267e
Serhii Kupriienko <skupriienko@anaconda.com>
2024-10-10 18:11:35 +0300

    jq_1.7.1

---
+++ recipe/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ex
+
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* ./modules/oniguruma
+cp $BUILD_PREFIX/share/gnuconfig/config.* ./config
+
+chmod +x configure
+
+./configure \
+    --prefix=$PREFIX \
+	--with-oniguruma=$PREFIX \
+	--enable-shared \
+    --disable-docs
+
+make -j${CPU_COUNT}
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+make check
+fi
+
+make install
