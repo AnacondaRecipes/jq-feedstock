006bbedadc15843ec72c3105417b38e87e8ab991
Serhii Kupriienko <skupriienko@anaconda.com>
2024-10-11 10:32:03 +0300

    Add CFLAGS with -fstack-protector-all, make tests verbose

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,15 +8,18 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* ./config
 
 chmod +x configure
 
+export CFLAGS="-O2 -pthread -fstack-protector-all ${CFLAGS}"
+
 ./configure \
-    --prefix=$PREFIX \
+	--prefix=$PREFIX \
 	--with-oniguruma=$PREFIX \
 	--enable-shared \
-    --disable-docs
+	--disable-docs \
+	--disable-valgrind
 
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-make check
+make check VERBOSE=yes
 fi
 
 make install
