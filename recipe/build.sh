b4c2686a89f9d39dba416de2f43fe7f8acc00f14
Serhii Kupriienko <skupriienko@anaconda.com>
2024-10-15 15:55:18 +0300

    Rewrite patch_libtool

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,11 +6,14 @@ set -exuo pipefail
 # unset the SUBDIR variable since it changes the behavior of make here
 unset SUBDIR
 
-declare -a _CONFIG_OPTS=()
-
 if [[ ${target_platform} == win-64 ]]
 then
 	export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}
+	export CFLAGS="-O2 -fstack-protector-all ${CFLAGS}"
+    # Get an updated config.sub and config.guess
+	cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./modules/oniguruma
+	cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
+
 	# unistd.h is included in ${PREFIX}/include/zconf.h
 	if [[ ! -f "${PREFIX}/include/unistd.h" ]]; then
 		UNISTD_CREATED=1
@@ -24,16 +27,9 @@ then
 		--with-oniguruma=$PREFIX \
 		--enable-shared \
 		--disable-docs \
-		--disable-valgrind \
-		"${_CONFIG_OPTS[@]}" || (cat config.log && false)
-	
-	sed -i.bak "s/export_symbols_cmds=/export_symbols_cmds2=/g" libtool
-    sed "s/archive_expsym_cmds=/archive_expsym_cmds2=/g" libtool > libtool2
-    echo "#!/bin/bash" > libtool
-    echo "export_symbols_cmds=\"$SRC_DIR/create_def.sh \\\$export_symbols \\\$libobjs \\\$convenience \"" >> libtool
-    echo "archive_expsym_cmds=\"\\\$CC -o \\\$tool_output_objdir\\\$soname \\\$libobjs \\\$compiler_flags \\\$deplibs -Wl,-DEF:\\\\\\\"\\\$export_symbols\\\\\\\" -Wl,-DLL,-IMPLIB:\\\\\\\"\\\$tool_output_objdir\\\$libname.dll.lib\\\\\\\"; echo \"" >> libtool
-    cat libtool2 >> libtool
-    sed -i.bak "s@|-fuse@|-fuse-ld=*|-nostdlib|-fuse@g" libtool
+		--disable-valgrind
+
+	patch_libtool
 else
     export CFLAGS="-O2 -pthread -fstack-protector-all ${CFLAGS}"
     # Get an updated config.sub and config.guess
@@ -46,8 +42,7 @@ else
 		--with-oniguruma=$PREFIX \
 		--enable-shared \
 		-disable-docs \
-		--disable-valgrind \
-		"${_CONFIG_OPTS[@]}"
+		--disable-valgrind
 fi
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
