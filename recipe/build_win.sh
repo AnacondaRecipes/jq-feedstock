79c1b393750a8cf847fd562445a126ad14217fad
Eric Lundby <Eric.Lundby@gmail.com>
2026-03-31 16:58:21 -0600

    Update build_win.sh

--- recipe/build_win.sh
+++ recipe/build_win.sh
@@ -2,6 +2,12 @@
 
 chmod +x configure
 HOST=--host=x86_64-w64-mingw32
+UCRT_PREFIX="${UCRT_PREFIX:-$PREFIX}"
+
+# MinGW links libwinpthread dynamically by default; conda-build then requires
+# ucrt64-libwinpthread-git. Static-link winpthread so jq.exe/libjq do not NEEDED it.
+export LDFLAGS="${LDFLAGS} -Wl,-Bstatic -lwinpthread -Wl,-Bdynamic"
+
 pushd vendor/oniguruma
   autoreconf -vfi
 popd
@@ -10,6 +16,7 @@ popd
             --prefix=$UCRT_PREFIX  \
             --with-oniguruma=$PREFIX/Library  \
             ${HOST}
-make LDFLAGS=-all-static -j${CPU_COUNT}
+
+make -j${CPU_COUNT}
 make check
 make install
