8d68e3e433bb343022fc474ac230b8e2a20da288
Eric Lundby <Eric.Lundby@gmail.com>
2026-04-01 08:30:29 -0600

    Revert "Update build_win.sh"
    This reverts commit 79c1b393750a8cf847fd562445a126ad14217fad.

--- recipe/build_win.sh
+++ recipe/build_win.sh
@@ -2,12 +2,6 @@
 
 chmod +x configure
 HOST=--host=x86_64-w64-mingw32
-UCRT_PREFIX="${UCRT_PREFIX:-$PREFIX}"
-
-# MinGW links libwinpthread dynamically by default; conda-build then requires
-# ucrt64-libwinpthread-git. Static-link winpthread so jq.exe/libjq do not NEEDED it.
-export LDFLAGS="${LDFLAGS} -Wl,-Bstatic -lwinpthread -Wl,-Bdynamic"
-
 pushd vendor/oniguruma
   autoreconf -vfi
 popd
@@ -16,7 +10,6 @@ popd
             --prefix=$UCRT_PREFIX  \
             --with-oniguruma=$PREFIX/Library  \
             ${HOST}
-
-make -j${CPU_COUNT}
+make LDFLAGS=-all-static -j${CPU_COUNT}
 make check
 make install
